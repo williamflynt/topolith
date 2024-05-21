d198d5b947317a2792764e5c787083e9a799d788
williamflynt <williamflynt>
2024-05-21 11:58:32 -0700

    chore(core): Update comment; change IsExternal to External

--- pkg/core/item.go
+++ pkg/core/item.go
@@ -1,11 +1,10 @@
 package core
 
-// ItemType is an iota that represents the different characteristics of an Item.
+// ItemType is an iota that represents the type of Item.
 type ItemType int
 
 const (
 	_ ItemType = iota
-	External
 	Person
 	Database
 	Queue
@@ -19,10 +18,10 @@ const (
 
 // Item is a struct that represents a single entity in the world. If using the C4 diagrams methodology, an Item can be any of Person, Software System, Container, Component, or Code element.
 type Item struct {
-	Id         string   `json:"id"`         // Id is the unique identifier of the Item. Often the same as Name.
-	IsExternal bool     `json:"isExternal"` // IsExternal is a boolean that represents whether the Item is external - a C4 diagrams concept.
-	Type       ItemType `json:"type"`       // Type is the type of the Item.
-	Name       string   `json:"name"`       // Name is the display name of the Item; meant for humans.
-	Mechanism  string   `json:"mechanism"`  // Mechanism is the method of implementation of the Item. This may not always be relevant for a strict C4 diagram.
-	Expanded   string   `json:"expanded"`   // Expanded is the expanded description of the Item. This may not always be relevant for a strict C4 diagram.
+	Id        string   `json:"id"`        // Id is the unique identifier of the Item. Often the same as Name.
+	External  bool     `json:"external"`  // External is a boolean that represents whether the Item is external - a C4 diagrams concept.
+	Type      ItemType `json:"type"`      // Type is the type of the Item.
+	Name      string   `json:"name"`      // Name is the display name of the Item; meant for humans.
+	Mechanism string   `json:"mechanism"` // Mechanism is the method of implementation of the Item. This may not always be relevant for a strict C4 diagram.
+	Expanded  string   `json:"expanded"`  // Expanded is the expanded description of the Item. This may not always be relevant for a strict C4 diagram.
 }
