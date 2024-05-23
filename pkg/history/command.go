26c8ea71a2c687e4389d95e844f267468a3490a9
williamflynt <williamflynt>
2024-05-23 11:23:49 -0700

    wip(history): WIP 5.2 - add string and parse funcs for params

--- pkg/history/command.go
+++ pkg/history/command.go
@@ -3,7 +3,11 @@ package history
 import (
 	"fmt"
 	"github.com/williamflynt/topolith/pkg/topolith"
+	"regexp"
+	"strings"
 )
+
+var kvPattern = regexp.MustCompile(`\b(\w+)="?(\w+)"?\b`)
 
 // CommandVerb represents the action to be performed.
 type CommandVerb string
@@ -47,20 +51,22 @@ type ItemCreateCommand struct {
 }
 
 func (c *ItemCreateCommand) Execute(w topolith.World) error {
-	// Perform create operation
-	fmt.Printf("Creating %v with Id %s\n", c.ResourceType, c.Id)
-	return nil
+	w.ItemCreate(c.Id, c.Params)
+	return w.Err()
 }
 
 func (c *ItemCreateCommand) Undo(w topolith.World) error {
-	// Undo create operation
-	fmt.Printf("Undo creating %v with Id %s\n", c.ResourceType, c.Id)
-	// TODO Implement
-	return nil
+	// TODO(wf 23 May 2024): What if I call create multiple times, then undo the most recent?
+	//  I would expect, as a user, that the state of World would be whatever it was before,
+	//  which means undoing the second, redundant create wouldn't change anything.
+	//  But here we delete it. How to handle?
+	w.ItemDelete(c.Id)
+	return w.Err()
 }
 
 func (c *ItemCreateCommand) String() string {
-	return fmt.Sprintf("Create %v with Id %s", c.ResourceType, c.Id)
+
+	return fmt.Sprintf("%s %s %s", c.ResourceType, c.Id)
 }
 
 func (c *ItemCreateCommand) FromString(s string) (Command, error) {
@@ -162,19 +168,19 @@ type ItemFreeCommand struct {
 
 func (c *ItemFreeCommand) Execute(w topolith.World) error {
 	// Perform nest operation
-	fmt.Printf("Nesting Item with Id %s under Item with Id %s\n", c.Id)
+	fmt.Printf("Freeing Item with Id %s to root\n", c.Id)
 	return nil
 }
 
 func (c *ItemFreeCommand) Undo(w topolith.World) error {
 	// Undo nest operation
-	fmt.Printf("Undo nesting Item with Id %s under Item with Id %s\n", c.Id)
+	fmt.Printf("Undo freeing Item with Id %s under Item with Id %s\n", c.Id, c.OldParentId)
 	// TODO Implement
 	return nil
 }
 
 func (c *ItemFreeCommand) String() string {
-	return fmt.Sprintf("Nest Item with Id %s under Item with Id %s", c.Id)
+	return fmt.Sprintf("free Item with Id %s", c.Id)
 }
 
 func (c *ItemFreeCommand) FromString(s string) (Command, error) {
@@ -263,4 +269,118 @@ func (c *RelDeleteCommand) String() string {
 func (c *RelDeleteCommand) FromString(s string) (Command, error) {
 	// TODO Implement
 	panic("not implemented")
+}
+
+// --- INTERNAL FUNCTIONS ---
+
+func itemParamsToString(params topolith.ItemSetParams) string {
+	components := make([]string, 0)
+	if params.External != nil {
+		components = append(components, fmt.Sprintf(`external=%t`, *params.External))
+	}
+	if params.Name != nil {
+		components = append(components, fmt.Sprintf(`name="%s"`, *params.Name))
+	}
+	if params.Type != nil {
+		components = append(components, fmt.Sprintf(`type="%s"`, *params.Type))
+	}
+	if params.Mechanism != nil {
+		components = append(components, fmt.Sprintf(`mechanism="%s"`, *params.Mechanism))
+	}
+	if params.Expanded != nil {
+		components = append(components, fmt.Sprintf(`expanded="%s"`, *params.Expanded))
+	}
+	return strings.Join(components, " ")
+}
+
+func itemParamsFromString(s string) (topolith.ItemSetParams, error) {
+	elements := kvPattern.FindAllStringSubmatch(s, -1)
+	params := topolith.ItemSetParams{}
+	for _, element := range elements {
+		key := element[1]
+		value := strings.Trim(element[2], `'"`)
+		switch key {
+		case "external":
+			external := value == "true"
+			params.External = &external
+		case "name":
+			params.Name = &value
+		case "type":
+			if strings.EqualFold(value, string(ItemTarget)) {
+				// Avoid weird lower-casing and set directly.
+				v := string(ItemTarget)
+				params.Type = &v
+			}
+			if strings.EqualFold(value, string(RelTarget)) {
+				v := string(RelTarget)
+				params.Type = &v
+			}
+		case "mechanism":
+			params.Mechanism = &value
+		case "expanded":
+			params.Expanded = &value
+		default:
+			return params, fmt.Errorf("unknown key %s", key)
+		}
+	}
+	return params, nil
+}
+
+func relParamsToString(params topolith.RelSetParams) string {
+	components := make([]string, 0)
+	if params.Verb != nil {
+		components = append(components, fmt.Sprintf(`verb="%s"`, *params.Verb))
+	}
+	if params.Mechanism != nil {
+		components = append(components, fmt.Sprintf(`mechanism="%s"`, *params.Mechanism))
+	}
+	if params.Async != nil {
+		components = append(components, fmt.Sprintf(`async=%t`, *params.Async))
+	}
+	if params.Expanded != nil {
+		components = append(components, fmt.Sprintf(`expanded="%s"`, *params.Expanded))
+	}
+	return strings.Join(components, " ")
+}
+
+func relParamsFromString(s string) (topolith.RelSetParams, error) {
+	elements := kvPattern.FindAllStringSubmatch(s, -1)
+	params := topolith.RelSetParams{}
+	for _, element := range elements {
+		key := element[1]
+		value := strings.Trim(element[2], `'"`)
+		switch key {
+		case "verb":
+			if strings.EqualFold(value, string(Create)) {
+				v := string(Create)
+				params.Verb = &v
+			}
+			if strings.EqualFold(value, string(Set)) {
+				v := string(Set)
+				params.Verb = &v
+			}
+			if strings.EqualFold(value, string(Delete)) {
+				v := string(Delete)
+				params.Verb = &v
+			}
+			if strings.EqualFold(value, string(Nest)) {
+				v := string(Nest)
+				params.Verb = &v
+			}
+			if strings.EqualFold(value, string(Free)) {
+				v := string(Free)
+				params.Verb = &v
+			}
+		case "mechanism":
+			params.Mechanism = &value
+		case "async":
+			async := value == "true"
+			params.Async = &async
+		case "expanded":
+			params.Expanded = &value
+		default:
+			return params, fmt.Errorf("unknown key %s", key)
+		}
+	}
+	return params, nil
 }
