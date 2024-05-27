db0c13f09ca07d9303833f13b751d766a7753180
williamflynt <williamflynt>
2024-05-27 10:21:33 -0700

    feat(grammar): Update main.go

--- pkg/grammar/main.go
+++ pkg/grammar/main.go
@@ -1,19 +1,30 @@
 package grammar
 
+import "strings"
+
+// TODO(wf 27 May 2024): We shouldn't be setting raw strings
+//  (ex: `Flags` -> []Flag)
+//  Make ResourceType, Flag, etc... constants
+//  Then work those into the `.peg` file (the Go code parts).
+
 // InputAttributes is a struct that holds information from the parsed input to the REPL.
 type InputAttributes struct {
 	ResourceType string            `json:"resourceType"`
 	ResourceId   string            `json:"resourceId"`
-	SecondaryId  string            `json:"secondaryId"`
+	ResourceIds  []string          `json:"resourceIds"`
+	SecondaryIds []string          `json:"secondaryIds"`
 	Verb         string            `json:"verb"`
 	Params       map[string]string `json:"params"`
-	Strict       bool              `json:"strict"`
+	Flags        []string          `json:"flags"`
 }
 
 // Response is a struct that holds the response from our grammar.
 type Response struct {
-	Object string `json:"object"`
-	Error  struct {
+	Object struct {
+		Type string `json:"type"`
+		Repr string `json:"repr"`
+	} `json:"object"`
+	Error struct {
 		Code    int    `json:"code"`
 		Message string `json:"message"`
 	} `json:"error"`
@@ -21,12 +32,28 @@ type Response struct {
 
 // Parse function to validate and pull information from the input to the REPL.
 func Parse(s string) (*Parser, error) {
-	p := &Parser{Buffer: s}
+	p := &Parser{
+		Buffer: s,
+		InputAttributes: InputAttributes{
+			ResourceIds:  make([]string, 0),
+			SecondaryIds: make([]string, 0),
+			Verb:         "",
+			Params:       make(map[string]string),
+			Flags:        make([]string, 0),
+		},
+	}
 	if err := p.Init(); err != nil {
 		return p, err
 	}
 	if err := p.Parse(); err != nil {
 		return p, err
 	}
+	p.Execute()
 	return p, nil
+}
+
+// --- INTERNAL ---
+
+func cleanString(s string) string {
+	return strings.Trim(strings.TrimSpace(s), "\"")
 }
